3895427479b33a1b5951482d7d8748a09b22baa7
tranphuquy19 <tranphuquy19@gmail.com>
2019-10-14 02:11:48 +0700

    clean code

--- src/main/java/tk/doraneko/tcp/Default.java
+++
@@ -1,8 +0,0 @@
-package tk.doraneko.tcp;
-
-/**
- * Created by @tranphuquy19 on 13/10/2019
- * Email:       tranphuquy19@gmail.com
- */
-public class Default {
-}
--- src/main/java/tk/doraneko/udp/Default.java
+++
@@ -1,8 +0,0 @@
-package tk.doraneko.udp;
-
-/**
- * Created by @tranphuquy19 on 13/10/2019
- * Email:       tranphuquy19@gmail.com
- */
-public class Default {
-}
