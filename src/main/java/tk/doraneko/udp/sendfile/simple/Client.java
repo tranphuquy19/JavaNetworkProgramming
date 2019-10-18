18302ed7e906d4814349b47c1fedc05901d83a8a
tranphuquy19 <tranphuquy19@gmail.com>
2019-10-18 14:18:07 +0700

    int udp send file

---
+++ src/main/java/tk/doraneko/udp/sendfile/simple/Client.java
@@ -0,0 +1,8 @@
+package tk.doraneko.udp.sendfile.simple;
+
+/**
+ * @author tranphuquy19@gmail.com
+ * @since 18/10/2019
+ */
+public class Client {
+}
---
+++ src/main/java/tk/doraneko/udp/sendfile/simple/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Ứng dụng gửi file sử dụng UDP
+ * @author tranphuquy19@gmail.com
+ * @since 18/10/2019
+ */
+package tk.doraneko.udp.sendfile.simple;
