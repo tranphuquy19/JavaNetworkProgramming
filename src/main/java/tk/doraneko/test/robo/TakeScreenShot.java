1ca67dd40ed49953901fbf422b18441e21ed4138
tranphuquy19 <tranphuquy19@gmail.com>
2019-10-31 23:51:22 +0700

    benchmark take screenshot

---
+++ src/main/java/tk/doraneko/test/robo/TakeScreenShot.java
@@ -0,0 +1,42 @@
+package tk.doraneko.test.robo;
+
+import com.sun.imageio.plugins.common.ImageUtil;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * @author tranphuquy19@gmail.com
+ * @since 31/10/2019
+ */
+public class TakeScreenShot {
+    public static void main(String[] args) throws AWTException, IOException {
+//        long startTime = System.currentTimeMillis();
+//        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//        ImageIO.write(image,"jpg", byteArrayOutputStream);
+//        byte[] imagrArr = byteArrayOutputStream.toByteArray();
+//        long time = System.currentTimeMillis() - startTime;
+//        System.out.println("asd");
+        Rectangle rect = new Rectangle( Toolkit.getDefaultToolkit().getScreenSize() );
+        try {
+            Robot robot = new Robot();
+            int count = 1;
+            long beforeTime = System.currentTimeMillis();
+            while ( count < 20 ) {
+                BufferedImage buffImage = robot.createScreenCapture( rect );
+                ByteArrayOutputStream bios = new ByteArrayOutputStream();
+                ImageIO.write(buffImage, "jpg", bios);
+                byte[] imgArr = bios.toByteArray();
+                count++;
+            }
+            double time = System.currentTimeMillis() - beforeTime;
+            System.out.println( "Seconds it took for 32 screen captures: " + time / 1000 );
+        } catch ( Exception e ) { }
+
+    }
+}
